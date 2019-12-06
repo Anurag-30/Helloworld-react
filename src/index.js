1bea93654a5a9b23361e4ced572a488611a0e0eb
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2019-12-06 15:16:15 +0530

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <h1 className="text">  To Do list</h1>,
-  
-  <button type="button" onclick="alert('Hello world!')">submit!</button>,
-  <div>
-    <p>TASKS</p>
-
-  </div>,
+  ,
   document.getElementById('root')
  
 );
