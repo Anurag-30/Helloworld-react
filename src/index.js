a5621f7580426e8fe0d21ac52e29aad3cfc726a1
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2019-12-06 16:53:51 +0530

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function App(props) {
-  return (<div>
-        <h1>React TODO</h1>
-        <h2>Tasks</h2>
-        <ToDoItems />
-      </div>
-  );
-}
-import React from "react";
+ReactDOM.render(
+  <h1 className="text">  To Do list</h1>,
+  <div>
+    <p>TASKS</p>
+    
+  </div>,
+  document.getElementById('root')
+ 
+);
 
-function ToDoItem(props) {
-    return <div>{props.title}</div>
-}
-
-const todoItems = ["Learn React", "Read Clean code", "Watch Movie"];
-
-const getToDoList = () => todoItems.map(itemTitle => <ToDoItem title={itemTitle}/>);
-
-export default getToDoList;
