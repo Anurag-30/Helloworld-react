0286efa5221faca605c037ef9fa90320d58f109a
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2019-12-06 13:16:24 +0530

    --- Auto-Git Commit ---

--- src/helloworld.js
+++ src/helloworld.js
@@ -12,19 +12,3 @@ ReactDOM.render(
 );
 
 
-/* 
-OR you can use the following syntax too.
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  React.createElement(
-      'h1',
-      {},
-      'Hello, world!'
-  ),
-  document.getElementById('root')
-); */
