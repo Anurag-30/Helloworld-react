6f9e4455c01dd6f3f8ed564736afc7f0be473efd
Anuragp <anuragp@Anuragps-MacBook-Pro.local>
2019-12-06 16:57:07 +0530

    --- Auto-Git Commit ---

--- src/ToDoitems.js
+++ src/ToDoitems.js
@@ -2,3 +2,11 @@ import react from 'react';
 import ToDoitems from './ToDoitems';
 import './APP.css';
 
+function App(props) {
+    return (<div>
+          <h1>React TODO</h1>
+          <h2>Tasks</h2>
+          <ToDoItems />
+        </div>
+    );
+  }
